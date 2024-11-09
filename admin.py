fbd5815df023b1dfe0170271ac1b50f32660764a
Akupra13 <aleksejkupraschwili@gmail.com>
2024-11-09 19:05:32 +0100

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Task, SubTask, Category
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'deadline', 'created_at')
+    search_fields = ('title', 'status')
+    list_filter = ('status', 'categories')
+    ordering = ('-created_at',)
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task', 'status', 'deadline', 'created_at')
+    search_fields = ('title', 'task__title')
+    list_filter = ('status',)
+    ordering = ('-created_at',)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
